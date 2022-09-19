c23101abe87d078eb2aea5560bd4df5db900da94
Posandu Mapa <posandumapa@gmail.com>
2022-09-19 13:33:31 +0530

    🩹  fix some stuff

--- utils/theme.ts
+++ utils/theme.ts
@@ -1,14 +1,13 @@
 import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
 
 const config: ThemeConfig = {
-    initialColorMode: 'light',
     useSystemColorMode: true,
     cssVarPrefix: '_c',
 }
 
 const theme = extendTheme({
     config,
-    // other theme config
+    // other theme config,
 })
 
 export default theme
